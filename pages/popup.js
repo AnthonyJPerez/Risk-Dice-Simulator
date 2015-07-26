bb48959ee1c48227980ca8782d49b630e1cdae49
AJ Perez <AnthonyJPerez24@gmail.com>
2015-07-26 11:36:30 -0400

    Popup now sends basic simulation form data to the background script.

--- pages/popup.js
+++ pages/popup.js
@@ -5,9 +5,40 @@ var dom_loaded = new $.Deferred();
 $.when(dom_loaded).then(main);
 
 
+function convertFormToJson(form)
+{
+	var json = {};
+    
+    jQuery.each(form, function() {
+        json[this.name] = this.value || '';
+    });
+    
+    return json;
+}
+
+
+function runSimulation()
+{
+	console.log("Running the simulation");
+
+	// Grab the form parameters
+	var formArray = $("#simulationForm").serializeArray();
+	var json = convertFormToJson(formArray);
+	console.log("form JSON:", json);
+
+	chrome.runtime.sendMessage({"parameters":json});
+
+	// Don't reset the form.
+	return false;
+}
+
+
 function main()
 {
 	console.log("Main started.");
+
+	// Setup the onclick handler for our simulate button
+	$("#simulate").click(runSimulation);
 }
 
 
